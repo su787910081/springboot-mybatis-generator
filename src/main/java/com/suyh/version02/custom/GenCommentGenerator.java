65d29975ae04140e0f7dbefc243971062b02c825
suyh <su_787910081@sina.com>
2020-05-01 00:51:09 +0800

    从网上整理下来，接下来就是跑了试下。文件似乎路径不对。

---
+++ src/main/java/com/suyh/version02/custom/GenCommentGenerator.java
@@ -0,0 +1,162 @@
+package com.suyh.version02.custom;
+
+import org.mybatis.generator.api.CommentGenerator;
+import org.mybatis.generator.api.IntrospectedColumn;
+import org.mybatis.generator.api.IntrospectedTable;
+import org.mybatis.generator.api.dom.java.*;
+import org.mybatis.generator.api.dom.xml.XmlElement;
+import org.mybatis.generator.internal.util.StringUtility;
+
+import java.util.Properties;
+import java.util.Set;
+
+public class GenCommentGenerator implements CommentGenerator {
+    @Override
+    public void addConfigurationProperties(Properties properties) {
+
+    }
+
+    /**
+     * 给字段添加数据库备注
+     *
+     * @param field
+     * @param introspectedTable
+     * @param introspectedColumn
+     */
+    @Override
+    public void addFieldComment(Field field,
+                                IntrospectedTable introspectedTable,
+                                IntrospectedColumn introspectedColumn) {
+        if (StringUtility.stringHasValue(introspectedColumn.getRemarks())) {
+            field.addJavaDocLine("//" + introspectedColumn.getRemarks());
+        }
+    }
+
+    @Override
+    public void addFieldComment(Field field, IntrospectedTable introspectedTable) {
+
+    }
+
+    @Override
+    public void addModelClassComment(TopLevelClass topLevelClass, IntrospectedTable introspectedTable) {
+
+    }
+
+    @Override
+    public void addClassComment(InnerClass innerClass, IntrospectedTable introspectedTable) {
+
+    }
+
+    @Override
+    public void addClassComment(InnerClass innerClass, IntrospectedTable introspectedTable, boolean b) {
+
+    }
+
+    @Override
+    public void addEnumComment(InnerEnum innerEnum, IntrospectedTable introspectedTable) {
+
+    }
+
+    /**
+     * getter方法注释
+     *
+     * @param method
+     * @param introspectedTable
+     * @param introspectedColumn
+     */
+    @Override
+    public void addGetterComment(Method method, IntrospectedTable introspectedTable,
+                                 IntrospectedColumn introspectedColumn) {
+        StringBuilder sb = new StringBuilder();
+        method.addJavaDocLine("/**");
+        if (StringUtility.stringHasValue(introspectedColumn.getRemarks())) {
+            sb.append(" * 获取");
+            sb.append(introspectedColumn.getRemarks());
+            method.addJavaDocLine(sb.toString());
+            method.addJavaDocLine(" *");
+        }
+        sb.setLength(0);
+        sb.append(" * @return ");
+        sb.append(introspectedColumn.getActualColumnName());
+        if (StringUtility.stringHasValue(introspectedColumn.getRemarks())) {
+            sb.append(" - ");
+            sb.append(introspectedColumn.getRemarks());
+        }
+        method.addJavaDocLine(sb.toString());
+        method.addJavaDocLine(" */");
+    }
+
+    /**
+     * setter方法注释
+     *
+     * @param method
+     * @param introspectedTable
+     * @param introspectedColumn
+     */
+    @Override
+    public void addSetterComment(Method method, IntrospectedTable introspectedTable, IntrospectedColumn introspectedColumn) {
+        StringBuilder sb = new StringBuilder();
+        method.addJavaDocLine("/**");
+        if (StringUtility.stringHasValue(introspectedColumn.getRemarks())) {
+            sb.append(" * 设置");
+            sb.append(introspectedColumn.getRemarks());
+            method.addJavaDocLine(sb.toString());
+            method.addJavaDocLine(" *");
+        }
+        Parameter parm = method.getParameters().get(0);
+        sb.setLength(0);
+        sb.append(" * @param ");
+        sb.append(parm.getName());
+        if (StringUtility.stringHasValue(introspectedColumn.getRemarks())) {
+            sb.append(" ");
+            sb.append(introspectedColumn.getRemarks());
+        }
+        method.addJavaDocLine(sb.toString());
+        method.addJavaDocLine(" */");
+    }
+
+    @Override
+    public void addGeneralMethodComment(Method method, IntrospectedTable introspectedTable) {
+
+    }
+
+    @Override
+    public void addJavaFileComment(CompilationUnit compilationUnit) {
+
+    }
+
+    @Override
+    public void addComment(XmlElement xmlElement) {
+
+    }
+
+    @Override
+    public void addRootComment(XmlElement xmlElement) {
+
+    }
+
+    @Override
+    public void addGeneralMethodAnnotation(Method method, IntrospectedTable introspectedTable, Set<FullyQualifiedJavaType> set) {
+
+    }
+
+    @Override
+    public void addGeneralMethodAnnotation(Method method, IntrospectedTable introspectedTable, IntrospectedColumn introspectedColumn, Set<FullyQualifiedJavaType> set) {
+
+    }
+
+    @Override
+    public void addFieldAnnotation(Field field, IntrospectedTable introspectedTable, Set<FullyQualifiedJavaType> set) {
+
+    }
+
+    @Override
+    public void addFieldAnnotation(Field field, IntrospectedTable introspectedTable, IntrospectedColumn introspectedColumn, Set<FullyQualifiedJavaType> set) {
+
+    }
+
+    @Override
+    public void addClassAnnotation(InnerClass innerClass, IntrospectedTable introspectedTable, Set<FullyQualifiedJavaType> set) {
+
+    }
+}
